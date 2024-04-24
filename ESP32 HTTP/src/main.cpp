22a342da8b5d9ab164dc115a5a892f8e4935940b
RasmusK-M <s194337@student.dtu.dk>
2024-04-24 13:02:13 +0200

    Added post function wit h voltageread and process

--- ESP32 HTTP/src/main.cpp
+++ ESP32 HTTP/src/main.cpp
@@ -12,13 +12,14 @@
 #include <WiFi.h>
 #include <HTTPClient.h>
 #include <Arduino_JSON.h>
+# define PIN_BATT 39
 
 const char* ssid = "Rasmus iPhone";
 const char* password = "12345678";
 
 //Domain name with URL path or IP address with path
-const char* serverName = "https://internetradioapi.azurewebsites.net/radio/play";
-
+const char* getServerName = "https://internetradioapi.azurewebsites.net/radio/play";
+const char* postServerName = "https://internetradioapi.azurewebsites.net/radio/battery";
 // the following variables are unsigned longs because the time, measured in
 // milliseconds, will quickly become a bigger number than can be stored in an int.
 unsigned long lastTime = 0;
@@ -29,19 +30,33 @@ unsigned long timerDelay = 5000;
 
 
 
+// HTTP variables
+int postResponseCode;
 
 
-// Initialsing values for extracting and processing JSON data
+// Initialsing variables for extracting and processing JSON data
 String musicReadings;
-const char* url;
-const char* name;
 String oldUrlString;
 String oldNameString;
 String urlString;
 String nameString;
+String islayingString;
+String volumeString;
+const char* url;
+const char* name;
+const char* isplaying;
+const char* volume;
 
+// Initialsing variables for battery
+String batteryData;
+
+
+// Protoype functions
 String httpGETRequest(const char* serverName);
+int httpPost(String payload);
 void connectToWiFi(void);
+String processBatteryLevel(void);
+
 
 void setup() {
   Serial.begin(115200);
@@ -49,6 +64,7 @@ void setup() {
   connectToWiFi();
 
   Serial.println("Timer set to 5 seconds (timerDelay variable), it will take 5 seconds before publishing the first reading.");
+  
 }
 
 void loop() {
@@ -58,7 +74,7 @@ void loop() {
     if(WiFi.status()== WL_CONNECTED){
               
       // Getting data from web server
-      musicReadings = httpGETRequest(serverName);
+      musicReadings = httpGETRequest(getServerName);
       JSONVar data = JSON.parse(musicReadings);
   
       // Checcking the format of the JSONVAR
@@ -74,6 +90,9 @@ void loop() {
       // Extract values
       url = data[0]["url"];
       name = data[0]["name"];
+      isplaying = data[0]["isPlaying"];
+      volume = data[0]["volume"];
+
       
       // Convert to strings
       urlString = String(url);
@@ -101,10 +120,20 @@ void loop() {
       Serial.println(oldUrlString);
       Serial.print("Name: ");
       Serial.println(oldNameString);
+
+      Serial.print("isplaying: ");
+      Serial.println(isplaying);
+      Serial.print("Volume: ");
+      Serial.println(volume);
       
       // Use oldUrlString and oldNameString for playing music
 
       //Rasmus has deleted JSON stuff with sensorReadingsArr
+
+      // HTTP Post stuff
+      batteryData = processBatteryLevel();
+      Serial.println(batteryData);
+      postResponseCode = httpPost(batteryData);
 
     } else {
       Serial.println("WiFi Disconnected");
@@ -115,32 +144,53 @@ void loop() {
 
 String httpGETRequest(const char* serverName) {
   // Rasmus has deleted the Wifi client from the example code
-  HTTPClient http;
+  HTTPClient httpGet;
     
   // Establishing connection the the server
-  http.begin(serverName); 
+  httpGet.begin(serverName); 
   
   // Send HTTP GET request
-  int httpResponseCode = http.GET();
+  int httpResponseCode = httpGet.GET();
   
   String payload = "{}"; 
   
   if (httpResponseCode>0) {
     Serial.print("HTTP Response code: ");
     Serial.println(httpResponseCode);
-    payload = http.getString();
+    payload = httpGet.getString();
   }
   else {
     Serial.print("Error code: ");
     Serial.println(httpResponseCode);
   }
   // Free resources
-  http.end();
+  httpGet.end();
 
   return payload;
 }
 
+int httpPost(String payload){
+  HTTPClient httpPost;
+    
+      // Your Domain name with URL path or IP address with path
+      httpPost.begin(postServerName);
+      
+      httpPost.addHeader("Content-Type", "application/json");
+
+      int httpResponseCode = httpPost.POST(payload);
+     
+      Serial.print("HTTP Post response code: ");
+      Serial.println(httpResponseCode);
+        
+      // Free resources
+      httpPost.end();
+
+      return httpResponseCode;
+
+}
+
 void connectToWiFi(void){
+
   WiFi.begin(ssid, password);
   Serial.println("Connecting to WiFi");
   while(WiFi.status() != WL_CONNECTED) {
@@ -150,4 +200,14 @@ void connectToWiFi(void){
   Serial.println("");
   Serial.print("Connected to WiFi network with IP Address: ");
   Serial.println(WiFi.localIP());
+}
+
+
+String processBatteryLevel(void){
+  String levelString = "{\"level\":\"XX\"}";        // initialising generic level string
+  float voltage = analogRead(PIN_BATT)*3.3/4096*6;  // Reading an calculating voltage level
+  int8_t percent = (voltage/12.4)*100;              // Converting to percent
+  levelString.replace("XX", (String) percent);      // Adding the percent to the levelString
+
+  return levelString;
 }
