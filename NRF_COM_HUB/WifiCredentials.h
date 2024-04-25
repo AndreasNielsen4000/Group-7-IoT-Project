6839f9ec805896b9f396ba466ca8580fbeba5375
AndreasNielsen4000 <andreasnielsen4000@gmail.com>
2024-04-25 20:16:44 +0200

    IoT HUB for azure server

---
+++ NRF_COM_HUB/WifiCredentials.h
@@ -0,0 +1,11 @@
+class WifiCredentials {
+    public:
+        const static char *SSID;
+        const static char *PASSWORD;
+};
+
+/* Create a WifiCredentials.cpp with the following content:
+#include "WifiCredentials.h"
+const char *WifiCredentials::SSID = "SSID Name";
+const char *WifiCredentials::PASSWORD = "Password";
+*/
